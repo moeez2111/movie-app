52af60377a605fad8ed99bd22b8f693da0a84aec
Moeez Asim <moeezasim30@gmail.com>
2026-04-09 02:08:39 +0500

    fix: build errors

--- src/Hooks/useMovies.js
+++ src/Hooks/useMovies.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 const useMovies = () => {
-
   const [movies, setMovies]   = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError]     = useState("");
@@ -10,25 +9,19 @@ const useMovies = () => {
     setLoading(true);
     setError("");
     setMovies([]);
-
     try {
       const res = await fetch(
         `https://www.omdbapi.com/?s=${title}&apikey=${import.meta.env.VITE_OMDB_KEY}`
       );
-
       if (!res.ok) throw new Error("Server error aaya!");
-
       const data = await res.json();
-
       if (data.Response === "True") {
         setMovies(data.Search);
       } else {
         throw new Error("Koi movie nahi mili! 😢");
       }
-
     } catch (err) {
       setError(err.message);
-
     } finally {
       setLoading(false);
     }
@@ -37,4 +30,4 @@ const useMovies = () => {
   return { movies, loading, error, searchMovies };
 };
 
-export default useMovies; // ← yeh zaroori hai!
+export default useMovies;
